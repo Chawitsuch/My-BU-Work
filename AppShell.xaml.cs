43c77b5d391e0f52d266ce2b079f2e423d9e6de4
Chawitsuch <89001775+Chawitsuch@users.noreply.github.com>
2021-10-21 11:21:34 +0700

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ProjectTAB.ViewModels;
+using ProjectTAB.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ProjectTAB
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
